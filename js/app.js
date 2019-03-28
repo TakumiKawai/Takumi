455efe968286380f908127ba761495bc8943ec5d
Takumi <borukichi171717@gmail.com>
2019-03-28 12:57:22 +0900

    「section」の表示のタイミングをjqとjsで揃えた。

--- js/app.js
+++ js/app.js
@@ -1,17 +1,36 @@
 $(function () {
 	
+	$('.now h3').fadeIn(3000);
+	$('.now p').fadeIn(3000);
 
-	$('header').on('click', function () {
-		// $('.now').removeClass('hide');
-		$('.now h3').fadeIn(3000);
-		$('.now p').fadeIn(3000);
-		$('.goal h3').fadeIn(4000);
-		$('.goal-sentence').fadeIn(2000);
-		$('.past h3').fadeIn(4000);
-		$('.past-sentence').fadeIn(2000);
-		$('.want h3').fadeIn(4000);
-		$('.want-sentence').fadeIn(2000);
-		$('.hope h3').fadeIn(4000);
-		$('.hope-sentence').fadeIn(2000);
+
+	let goal = $('.goal').offset().top -  400;
+	let past = $('.past').offset().top -  400;
+	let want = $('.want').offset().top -  400;
+	let hope = $('.hope').offset().top -  400;
+
+	$(window).on('scroll', function () {
+
+		
+
+		if ($(window).scrollTop() > goal) {
+			$('.goal h3').fadeIn(4000);
+			$('.goal-sentence').fadeIn(2000);			
+		}
+
+		if ($(window).scrollTop() > past) {
+			$('.past h3').fadeIn(4000);
+			$('.past-sentence').fadeIn(2000);
+		}
+
+		if ($(window).scrollTop() > want) {
+			$('.want h3').fadeIn(4000);
+			$('.want-sentence').fadeIn(2000);
+		}
+
+		if ($(window).scrollTop() > hope) {
+			$('.hope h3').fadeIn(4000);
+			$('.hope-sentence').fadeIn(2000);
+		}
 	})
 });

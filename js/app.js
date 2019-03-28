c86c08ac0e500af26fce961350f1e128aa49f9ac
Takumi <borukichi171717@gmail.com>
2019-03-28 11:43:58 +0900

    scriptに「fadeIn」、とclassにhideを入れました。

--- js/app.js
+++ js/app.js
@@ -5,5 +5,13 @@ $(function () {
 		// $('.now').removeClass('hide');
 		$('.now h3').fadeIn(3000);
 		$('.now p').fadeIn(3000);
+		$('.goal h3').fadeIn(4000);
+		$('.goal-sentence').fadeIn(2000);
+		$('.past h3').fadeIn(4000);
+		$('.past-sentence').fadeIn(2000);
+		$('.want h3').fadeIn(4000);
+		$('.want-sentence').fadeIn(2000);
+		$('.hope h3').fadeIn(4000);
+		$('.hope-sentence').fadeIn(2000);
 	})
 });

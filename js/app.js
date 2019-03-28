a89368bdbffa0c52a3e56b2ad79fc38de0703401
Takumi <borukichi171717@gmail.com>
2019-03-28 10:53:08 +0900

    「fadeIn」を入れました。

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ $(function () {
 	
 
 	$('header').on('click', function () {
-		$('.now').removeClass('hide');
+		// $('.now').removeClass('hide');
+		$('.now h3').fadeIn(3000);
+		$('.now p').fadeIn(3000);
 	})
 });
